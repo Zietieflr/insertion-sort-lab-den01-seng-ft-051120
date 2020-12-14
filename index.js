529e0cc939e7f1263699b17c7af22c58f4a3620f
Zietieflr <lrmcguire93@gmail.com>
2020-12-14 14:57:57 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function findMinAndRemove(array){
-
+  let minimum = array[0];
+  let minIndex = 0;
+  for(let i=1; i<array.length; i++) {
+    if (minimum > array[i]) {
+      minimum = array[i];
+      minIndex = i;
+    }
+  }
+  array.splice(minIndex, 1);
+  return minimum;
 }
 
 function selectionSort(array){
-
+  const sortedArray = [];
+  while (array.length !== 0) {
+    sortedArray.push(findMinAndRemove(array));
+  }
+  return sortedArray;
 }
